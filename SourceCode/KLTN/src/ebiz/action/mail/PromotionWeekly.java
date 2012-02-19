063ddaf07ddfc5b661520020f1626a965e0ecb6c
hailam349 <hailam349@gmail.com>
2012-02-19 06:41:03 +0000

    send mail

---
+++ SourceCode/KLTN/src/ebiz/action/mail/PromotionWeekly.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed to Open-Ones Group under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Open-Ones Group licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package ebiz.action.mail;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class PromotionWeekly extends Action {
+	private static final Logger log = Logger.getLogger(SendMailRegister.class.getName());
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+		// get promotion food
+//		List<FoodForm> formList = new ArrayList<FoodForm>();
+////		formList = FoodBLO.getFoodListByStatus(
+////				CommonConstant.HOME_NUMBER_DEFAUL, "1");
+//		// get customer list
+//		List<Customer> customerList = CustomerBLO.getCustomerList();
+//		for (Customer customer : customerList) {
+//			if (customer.getCustomerEmail() != null) {
+//				// birthday = current date
+//                if (CommonUtil.equalDateFormat(customer.getCustomerBirth(), new Date())) {
+//					StringBuffer msgBody = new StringBuffer();
+//					// design content of mail
+//					msgBody.append("<h1>Hiện tại trang web chúng tôi đang có các mặt hàng khuyến mãi hấp dẫn");
+//					msgBody.append("</h1>");
+//
+//					Properties props = new Properties();
+//					Session session = Session.getDefaultInstance(props, null);
+////					msgBody.append("<h1>Chúc mừng sinh nhật ");
+////					msgBody.append(customer.getCustomerId());
+////					msgBody.append("</h1>");
+//					try {
+//						Message msg = new MimeMessage(session);
+//                        msg.setFrom(new InternetAddress("uit.mmt@gmail.com", "Food.com"));
+//                        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(customer.getCustomerEmail(),
+//                                "User"));
+//						msg.setSubject("Sản phẩm khuyến mãi");
+//						msg.setContent(msgBody.toString(), "text/html");
+//						Transport.send(msg);
+//
+//					} catch (AddressException e) {
+//						// ...
+//					} catch (MessagingException e) {
+//						// ...
+//					}
+//
+//				}
+//			}
+//
+//		}
+    	 String msgBody;
+//       String flag=request.getParameter("flag");
+       String email = "hailam349@gmail.com";
+       Properties props = new Properties();
+       Session session = Session.getDefaultInstance(props, null);
+       msgBody ="Thong tin khuyen mai";
+       log.info("Send Mail Register Success");
+       try {
+           Message msg = new MimeMessage(session);
+           msg.setFrom(new InternetAddress("uit.mmt@gmail.com", "Food E-commerce.com"));
+               msg.addRecipient(Message.RecipientType.TO, new InternetAddress(email, "User"));
+           msg.setSubject("Daily Promotion Food");
+           msg.setText(msgBody);
+           Transport.send(msg);
+           
+
+       } catch (AddressException e) {
+           // ...
+       } catch (MessagingException e) {
+           // ...
+       }
+       return null;
+
+       }
+}
