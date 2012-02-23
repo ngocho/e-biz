fba9d356f5905af4b5f650390e901f30bd04edc6
kungdein <kungdein@gmail.com>
2012-02-23 10:06:20 +0000

    Remove some trunk code and unnecessary import

--- Study/TestCropImage/src/test/CropImageServlet.java
+++ Study/TestCropImage/src/test/CropImageServlet.java
@@ -1,18 +1,14 @@
 package test;
 
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet implementation class CropImageServlet
