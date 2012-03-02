84d88337eb4fdc7ebb04d4a5ddaea18c0e038981
kungdein <kungdein@gmail.com>
2012-03-02 02:51:11 +0000

    Commit Crop Image module in pop-upChange Crop module to popup

--- SourceCode/KLTN/src/ebiz/action/CropImage.java
+++ SourceCode/KLTN/src/ebiz/action/CropImage.java
@@ -18,6 +18,8 @@
  */
 package ebiz.action;
 
+import java.nio.ByteBuffer;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,34 +29,55 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.files.AppEngineFile;
+import com.google.appengine.api.files.FileService;
+import com.google.appengine.api.files.FileServiceFactory;
+import com.google.appengine.api.files.FileWriteChannel;
 import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesServiceFactory;
 import com.google.appengine.api.images.Transform;
 /**
  * Action Class for crop an image.
- *
  * @author CongDanh
+ *
  */
 public class CropImage extends Action {
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
         try {
-
             int leftX = Integer.parseInt(request.getParameter("leftX"));
             int topY = Integer.parseInt(request.getParameter("topY"));
             int rightX = Integer.parseInt(request.getParameter("rightX"));
             int bottomY = Integer.parseInt(request.getParameter("bottomY"));
+            double height = 1.0 * Integer.parseInt(request.getParameter("height"));
+            double width = 1.0 * Integer.parseInt(request.getParameter("width"));
             if ((leftX != rightX) && (topY != bottomY)) {
-                String urlKey = request.getSession().getAttribute("urlKey").toString();
+                String urlKey = request.getSession().getAttribute("urlImageKey").toString();
+                // get image
                 Image image = ImagesServiceFactory.makeImageFromBlob(new BlobKey(urlKey));
-                Transform crop = ImagesServiceFactory.makeCrop(leftX, topY, rightX, bottomY);
+                // crop image
+                Transform crop = ImagesServiceFactory.makeCrop(leftX/width, topY/height, rightX/width, bottomY/height);
                 ImagesService services = ImagesServiceFactory.getImagesService();
-                services.applyTransform(crop, image);
-                request.getSession().removeAttribute("urlImageKey");
+                Image newImage = services.applyTransform(crop, image);
+                
+                // Delete old file.
+                BlobstoreServiceFactory.getBlobstoreService().delete(new BlobKey(urlKey));
+                // save to File
+                FileService fileService = FileServiceFactory.getFileService();
+                AppEngineFile file = fileService.createNewBlobFile("image/png");
+                FileWriteChannel writeChannel = fileService.openWriteChannel(file, true);
+                ByteBuffer buffer = ByteBuffer.wrap(newImage.getImageData());
+                writeChannel.write(buffer);
+                writeChannel.closeFinally();
+                
+                // Get new blob key and write to session.
+                BlobKey blob = fileService.getBlobKey(file);
+                request.getSession().setAttribute("urlImageKey", blob.getKeyString());
                 request.getSession().removeAttribute("notCropped");
-                response.getWriter().print("success");
+                response.getWriter().print(blob.getKeyString());
             } else {
                 throw new NumberFormatException();
             }
