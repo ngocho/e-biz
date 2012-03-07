0ac22586c319683a5de2dd06cc7777ef8306a3e5
duyltgl92@gmail.com <duyltgl92@gmail.com@db352ffd-89e9-70db-57d8-02b6d358dc37>
2012-03-07 07:53:49 +0000

    fix mapping config

--- trunk/SourceCode/KLTN/src/ebiz/dto/account/provider/Provider.java
+++ trunk/SourceCode/KLTN/src/ebiz/dto/account/provider/Provider.java
@@ -1,20 +1,20 @@
 /**
- * Licensed to Open-Ones Group under one or more contributor license
- * agreements. See the NOTICE file distributed with this work
- * for additional information regarding copyright ownership.
- * Open-Ones Group licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a
- * copy of the License at:
+ *Licensed to Open-Ones Group under one or more contributor license
+ *agreements. See the NOTICE file distributed with this work
+ *for additional information regarding copyright ownership.
+ *Open-Ones Group licenses this file to you under the Apache License,
+ *Version 2.0 (the "License"); you may not use this file
+ *except in compliance with the License. You may obtain a
+ *copy of the License at:
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *Unless required by applicable law or agreed to in writing,
+ *software distributed under the License is distributed on
+ *an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *KIND, either express or implied. See the License for the
+ *specific language governing permissions and limitations
+ *under the License.
  */
 package ebiz.dto.account.provider;
 
@@ -27,315 +27,330 @@ import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
 /**
- * @author ThuyNT Provider
+ *@author ThuyNT Provider
  */
 @PersistenceCapable
 public class Provider implements Serializable {
-    /** . */
+    /**. */
     private static final long serialVersionUID = 1L;
-    /** . providerId */
+    /**. providerId */
     @PrimaryKey
     @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
     private String providerId;
-    /** . providerPassword */
+    /**. providerPassword */
     @Persistent
     private String providerPassword;
-    /** . providerName */
+    /**. providerName */
     @Persistent
     private String providerName;
-    /** . providerGender */
+    /**. providerGender */
     @Persistent
     private String providerGender;
-    /** . providerBirth */
+    /**. providerBirth */
     @Persistent
     private Date providerBirth;
-    /** . providerEmail */
+    /**. providerEmail */
     @Persistent
     private String providerEmail;
-    /** . providerPhone */
+    /**. providerPhone */
     @Persistent
     private String providerPhone;
-    /** . providerAddress */
+    /**. providerAddress */
     private String providerAddress;
-    /** . providerAccountPayment */
+    /**. providerAccountPayment */
     @Persistent
     private String providerAccountPayment;
-    /** . xuOnline */
+    /**. xuOnline */
     @Persistent
     private long xuOnline;
-    /** . authentication */
+    /**. authentication */
     @Persistent
     private String authentication;
-    /** . loginDate */
+    /**. loginDate */
     @Persistent
     private Date loginDate;
-    /** . dateRegister */
+    /**. dateRegister */
     @Persistent
     private Date dateRegister;
-    /** . detail */
+    /**. detail */
     @Persistent
     private String detail;
-    /** . isActive */
+    /**. url */
     private String url;
+    /**. isActive */
     @Persistent
     private boolean isActive; // required
+    /**. activecode */
     @Persistent
     private String activeCode; // required
     /**
-     * Get value of providerId.
-     * 
-     * @return the providerId
+     *Get value of providerId.
+     *
+     *@return the providerId
      */
     public String getProviderId() {
         return providerId;
     }
     /**
-     * Set the value for providerId.
-     * 
-     * @param providerId the providerId to set
+     *Set the value for providerId.
+     *
+     *@param providerId the providerId to set
      */
     public void setProviderId(String providerId) {
         this.providerId = providerId;
     }
     /**
-     * Get value of providerPassword.
-     * 
-     * @return the providerPassword
+     *Get value of providerPassword.
+     *
+     *@return the providerPassword
      */
     public String getProviderPassword() {
         return providerPassword;
     }
     /**
-     * Set the value for providerPassword.
-     * 
-     * @param providerPassword the providerPassword to set
+     *Set the value for providerPassword.
+     *
+     *@param providerPassword the providerPassword to set
      */
     public void setProviderPassword(String providerPassword) {
         this.providerPassword = providerPassword;
     }
     /**
-     * Get value of providerName.
-     * 
-     * @return the providerName
+     *Get value of providerName.
+     *
+     *@return the providerName
      */
     public String getProviderName() {
         return providerName;
     }
     /**
-     * Set the value for providerName.
-     * 
-     * @param providerName the providerName to set
+     *Set the value for providerName.
+     *
+     *@param providerName the providerName to set
      */
     public void setProviderName(String providerName) {
         this.providerName = providerName;
     }
     /**
-     * Get value of providerGender.
-     * 
-     * @return the providerGender
+     *Get value of providerGender.
+     *
+     *@return the providerGender
      */
     public String getProviderGender() {
         return providerGender;
     }
     /**
-     * Set the value for providerGender.
-     * 
-     * @param providerGender the providerGender to set
+     *Set the value for providerGender.
+     *
+     *@param providerGender the providerGender to set
      */
     public void setProviderGender(String providerGender) {
         this.providerGender = providerGender;
     }
     /**
-     * Get value of providerBirth.
-     * 
-     * @return the providerBirth
+     *Get value of providerBirth.
+     *
+     *@return the providerBirth
      */
     public Date getProviderBirth() {
         return providerBirth;
     }
     /**
-     * Set the value for providerBirth.
-     * 
-     * @param providerBirth the providerBirth to set
+     *Set the value for providerBirth.
+     *
+     *@param providerBirth the providerBirth to set
      */
     public void setProviderBirth(Date providerBirth) {
         this.providerBirth = providerBirth;
     }
     /**
-     * Get value of providerEmail.
-     * 
-     * @return the providerEmail
+     *Get value of providerEmail.
+     *
+     *@return the providerEmail
      */
     public String getProviderEmail() {
         return providerEmail;
     }
     /**
-     * Set the value for providerEmail.
-     * 
-     * @param providerEmail the providerEmail to set
+     *Set the value for providerEmail.
+     *
+     *@param providerEmail the providerEmail to set
      */
     public void setProviderEmail(String providerEmail) {
         this.providerEmail = providerEmail;
     }
     /**
-     * Get value of providerPhone.
-     * 
-     * @return the providerPhone
+     *Get value of providerPhone.
+     *
+     *@return the providerPhone
      */
     public String getProviderPhone() {
         return providerPhone;
     }
     /**
-     * Set the value for providerPhone.
-     * 
-     * @param providerPhone the providerPhone to set
+     *Set the value for providerPhone.
+     *
+     *@param providerPhone the providerPhone to set
      */
     public void setProviderPhone(String providerPhone) {
         this.providerPhone = providerPhone;
     }
     /**
-     * Get value of providerAddress.
-     * 
-     * @return the providerAddress
+     *Get value of providerAddress.
+     *
+     *@return the providerAddress
      */
     public String getProviderAddress() {
         return providerAddress;
     }
     /**
-     * Set the value for providerAddress.
-     * 
-     * @param providerAddress the providerAddress to set
+     *Set the value for providerAddress.
+     *
+     *@param providerAddress the providerAddress to set
      */
     public void setProviderAddress(String providerAddress) {
         this.providerAddress = providerAddress;
     }
     /**
-     * Get value of providerAccountPayment.
-     * 
-     * @return the providerAccountPayment
+     *Get value of providerAccountPayment.
+     *
+     *@return the providerAccountPayment
      */
     public String getProviderAccountPayment() {
         return providerAccountPayment;
     }
     /**
-     * Set the value for providerAccountPayment.
-     * 
-     * @param providerAccountPayment the providerAccountPayment to set
+     *Set the value for providerAccountPayment.
+     *
+     *@param providerAccountPayment the providerAccountPayment to set
      */
     public void setProviderAccountPayment(String providerAccountPayment) {
         this.providerAccountPayment = providerAccountPayment;
     }
     /**
-     * Get value of xuOnline.
-     * 
-     * @return the xuOnline
+     *Get value of xuOnline.
+     *
+     *@return the xuOnline
      */
     public long getXuOnline() {
         return xuOnline;
     }
     /**
-     * Set the value for xuOnline.
-     * 
-     * @param xuOnline the xuOnline to set
+     *Set the value for xuOnline.
+     *
+     *@param xuOnline the xuOnline to set
      */
     public void setXuOnline(long xuOnline) {
         this.xuOnline = xuOnline;
     }
     /**
-     * Get value of authentication.
-     * 
-     * @return the authentication
+     *Get value of authentication.
+     *
+     *@return the authentication
      */
     public String getAuthentication() {
         return authentication;
     }
     /**
-     * Set the value for authentication.
-     * 
-     * @param authentication the authentication to set
+     *Set the value for authentication.
+     *
+     *@param authentication the authentication to set
      */
     public void setAuthentication(String authentication) {
         this.authentication = authentication;
     }
     /**
-     * Get value of loginDate.
-     * 
-     * @return the loginDate
+     *Get value of loginDate.
+     *
+     *@return the loginDate
      */
     public Date getLoginDate() {
         return loginDate;
     }
     /**
-     * Set the value for loginDate.
-     * 
-     * @param loginDate the loginDate to set
+     *Set the value for loginDate.
+     *
+     *@param loginDate the loginDate to set
      */
     public void setLoginDate(Date loginDate) {
         this.loginDate = loginDate;
     }
 
     /**
-     * Get value of isActive.
-     * 
-     * @return the isActive
+     *Get value of isActive.
+     *
+     *@return the isActive
      */
     public boolean isActive() {
         return isActive;
     }
     /**
-     * Set the value for isActive.
-     * 
-     * @param isActive the isActive to set
+     *Set the value for isActive.
+     *
+     *@param isActive the isActive to set
      */
     public void setActive(boolean isActive) {
         this.isActive = isActive;
     }
     /**
-     * Get value of dateRegister.
-     * 
-     * @return the dateRegister
+     *Get value of dateRegister.
+     *
+     *@return the dateRegister
      */
     public Date getDateRegister() {
         return dateRegister;
     }
     /**
-     * Set the value for dateRegister.
-     * 
-     * @param dateRegister the dateRegister to set
+     *Set the value for dateRegister.
+     *
+     *@param dateRegister the dateRegister to set
      */
     public void setDateRegister(Date dateRegister) {
         this.dateRegister = dateRegister;
     }
     /**
-     * Get value of detail.
-     * 
-     * @return the detail
+     *Get value of detail.
+     *
+     *@return the detail
      */
     public String getDetail() {
         return detail;
     }
     /**
-     * Set the value for detail.
-     * 
-     * @param detail the detail to set
+     *Set the value for detail.
+     *
+     *@param detail the detail to set
      */
     public void setDetail(String detail) {
         this.detail = detail;
     }
+    /**
+     *
+     *Get active to active account.
+     *@return
+     *        active code
+     */
     public String getActiveCode() {
         return activeCode;
     }
+
+    /**
+     *
+     *set active code.
+     *@param activeCode
+     *       actice code
+     */
     public void setActiveCode(String activeCode) {
         this.activeCode = activeCode;
     }
     /**
-     * @return the url
+     *@return the url
      */
     public String getUrl() {
         return url;
     }
     /**
-     * @param url the url to set
+     *@param url the url to set
      */
     public void setUrl(String url) {
         this.url = url;
