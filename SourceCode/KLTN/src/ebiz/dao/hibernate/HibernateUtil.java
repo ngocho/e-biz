b21ef6ffc3850e1f0bb2c0b33b51ed5a94a46188
duyltgl92 <duyltgl92@gmail.com>
2012-03-20 04:01:08 +0000

    add HibernateUtil ( helper class - use to create a session instant)

---
+++ SourceCode/KLTN/src/ebiz/dao/hibernate/HibernateUtil.java
@@ -0,0 +1,22 @@
+package ebiz.dao.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    static {
+    try {
+    sessionFactory = new Configuration().configure()
+    .buildSessionFactory();
+    } catch (Throwable ex) {
+    System.err.println("Initial SessionFactory creation failed." + ex);
+    throw new ExceptionInInitializerError(ex);
+    }
+    }      
+
+    public static SessionFactory getSessionFactory() {
+        // TODO Auto-generated method stub
+        return sessionFactory;
+    }
+}
