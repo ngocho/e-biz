17ad0767370aaa57ad2bda9a7a76b4dd32e19f09
duyltgl92 <duyltgl92@gmail.com>
2012-03-20 04:06:58 +0000

    fix checkstyle of HibernateUtil.java file

--- SourceCode/KLTN/src/ebiz/dao/hibernate/HibernateUtil.java
+++ SourceCode/KLTN/src/ebiz/dao/hibernate/HibernateUtil.java
@@ -3,20 +3,36 @@ package ebiz.dao.hibernate;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * Helper class.
+ * Use to create session factory
+ * @author DuyLT60571
+ *
+ */
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
+
+    /**
+     * Session factory use to return out.
+     */
+    private static final SessionFactory SESSIONFACTORY;
     static {
     try {
-    sessionFactory = new Configuration().configure()
-    .buildSessionFactory();
+        SESSIONFACTORY = new Configuration().configure()
+                             .buildSessionFactory();
     } catch (Throwable ex) {
-    System.err.println("Initial SessionFactory creation failed." + ex);
-    throw new ExceptionInInitializerError(ex);
+        System.err.println("Initial SessionFactory creation failed." + ex);
+        throw new ExceptionInInitializerError(ex);
+        }
     }
-    }      
 
+    /**
+     * Use to return session factory object
+     * [Give the description for method].
+     * @return
+     *          SessionFactory
+     */
     public static SessionFactory getSessionFactory() {
         // TODO Auto-generated method stub
-        return sessionFactory;
+        return SESSIONFACTORY;
     }
 }
