f62786bc5e26dd910f3a2747982f1213af94f530
duyltgl92 <duyltgl92@gmail.com>
2012-03-21 07:53:55 +0000

    add save,update, delete  function

--- SourceCode/KLTN/src/ebiz/dao/hibernate/HibernateHelper.java
+++ SourceCode/KLTN/src/ebiz/dao/hibernate/HibernateHelper.java
@@ -3,6 +3,8 @@ package ebiz.dao.hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import ebiz.dto.account.admin.Admin;
+
 /**
  * Provide some basic hibernate action.
  * @author DuyLT60571
@@ -33,6 +35,40 @@ public class HibernateHelper {
         return saveSuccess;
     }
     
-    
+    public static boolean deleteObject(Object obj) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        boolean saveSuccess = false;
+        try {
+        transaction = session.beginTransaction();
+        session.delete(obj);
+        saveSuccess = true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return saveSuccess;
+    }
+
+    public static boolean isAdmin(String id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        Admin admin = null;
+        try {
+        transaction = session.beginTransaction();
+        admin = (Admin) session.get(Admin.class, id);
+        } catch (Exception e) {
+            System.out.print(e);
+        transaction.rollback();
+        e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return (admin != null);
+    }
 
 }
